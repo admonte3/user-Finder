a129fad6ae7b415cea5104011e066928c46b80c4
Adrian Montes <admonte3@gmail.com>
2019-09-08 19:23:40 -0400

    github user finder

---
+++ github.js
@@ -0,0 +1,23 @@
+class GitHub{
+    constructor(){
+        this.client_id = '7a4f8fd707d5580667d9';
+        this.client_secret = '83f6665f983b92e5cb0203f295cc6c21ee7ad433';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user){
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        //REPO resonse
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return{
+            profile,
+            repos
+        }
+    }
+}
